babe5cfbd96db02e09b37f56d4d48233f6f16761
AlitPurnama <alitpurnama13@gmail.com>
2025-02-02 03:13:46 +0900

    feat(ghostty): window padding

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -1,13 +1,16 @@
-{ lib, config, inputs, ... }:with lib;
-let
-  cfg = config.ghostty;
-in
-{
+{ lib, config, inputs, ... }:
+with lib;
+let cfg = config.ghostty;
+in {
   options.ghostty.enable = mkEnableOption "Enable Ghostty";
   config = mkIf cfg.enable {
     programs.ghostty = {
       enable = true;
       package = inputs.ghostty.packages.x86_64-linux.default;
+      settings = {
+        window-padding-x = 2;
+        window-padding-y = 2;
+      };
     };
   };
 }
