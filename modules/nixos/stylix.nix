acfae13aab9641d250f3103251cfc5e46d0a7b39
AlitPurnama <alitpurnama13@gmail.com>
2025-02-02 03:14:44 +0900

    feat(stylix): cursor, terminal
    - change cursor to bibata modern ice
    - change cursor size
    - change terminal font size

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,7 +1,14 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, ... }: {
   stylix.enable = true;
   stylix.image = ../../wallpapers/dragon.png;
   stylix.polarity = "dark";
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+  stylix.base16Scheme =
+    "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
 
+  stylix.fonts = { sizes.terminal = 20; };
+  stylix.cursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 28;
+  };
 }
