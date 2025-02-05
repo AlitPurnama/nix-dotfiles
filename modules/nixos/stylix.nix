8be0ace1b6e97c1ab9a8383b62279b9da1f20741
AlitPurnama <alitpurnama13@gmail.com>
2025-02-05 18:30:02 +0900

    chores(stylix.nix): change cursor size

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -4,11 +4,10 @@
   stylix.polarity = "dark";
   stylix.base16Scheme =
     "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-
   stylix.fonts = { sizes.terminal = 20; };
   stylix.cursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
-    size = 28;
+    size = 24;
   };
 }
