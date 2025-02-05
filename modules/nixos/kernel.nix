0f397cfffbfa851eaabe28182938da9334bfced1
AlitPurnama <alitpurnama13@gmail.com>
2025-02-05 13:58:41 +0900

    gk jadi

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -1,4 +1,1 @@
-{ config, pkgs, ... }: {
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.extraModulePackages = with config.boot.kernelPackages; [ wireguard ];
-}
+{  pkgs, ... }: { boot.kernelPackages = pkgs.linuxPackages_zen; }
