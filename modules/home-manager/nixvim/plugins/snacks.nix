be608214ea131b6ce899fedb04acca62a69446e8
AlitPurnama <alitpurnama13@gmail.com>
2025-02-06 21:55:16 +0900

    change(nixvim): dashboard icon

--- modules/home-manager/nixvim/plugins/snacks.nix
+++ modules/home-manager/nixvim/plugins/snacks.nix
@@ -17,24 +17,96 @@ in {
     programs.nixvim.plugins.snacks = {
       enable = true;
       settings = {
+        pick.enabled = true;
         dashboard = {
           enabled = cfg.enableDashboard;
+          preset = {
+            keys = [
+              {
+                icon = " ";
+                key = "f";
+                desc = "Find File";
+                action = "<leader>ff";
+              }
+              {
+                icon = " ";
+                key = "n";
+                desc = "New File";
+                action = ":ene | startinsert";
+              }
+              {
+                icon = " ";
+                key = "/";
+                desc = "Find Text";
+                action = "<leader>fr";
+              }
+              {
+                icon = " ";
+                key = "r";
+                desc = "Recent Files";
+                action = "<leader>fg";
+              }
+              {
+                icon = "";
+                key = "o";
+                desc = "LazyGit";
+                action = "<leader>gg";
+              }
+              {
+                icon = " ";
+                key = "q";
+                desc = "Quit";
+                action = ":qa";
+              }
+            ];
+          };
           sections = [
-            { section = "header"; }
-            {
-              section = "keys";
-              gap = 1;
-              padding = 1;
-            }
-            { section = "startup"; }
             {
               section = "terminal";
               cmd =
-                "${pkgs.ascii-image-converter}/bin/ascii-image-converter ~/.dotfiles/icons/Rimuru.Tempest.1024.2619740.png -C -c";
+                "${pkgs.ascii-image-converter}/bin/ascii-image-converter ~/.dotfiles/icons/le-rimuru.png -C -c";
               random = 10;
-              pane = 2;
+              pane = 1;
               indent = 4;
               height = 30;
+            }
+            {
+              icon = " ";
+              pane = 2;
+              title = "Keymaps";
+              section = "keys";
+              padding = 1;
+              indent = 3;
+            }
+            {
+              icon = " ";
+              pane = 2;
+              title = "Recent Files";
+              section = "recent_files";
+              padding = 1;
+              indent = 3;
+            }
+            {
+              icon = " ";
+              pane = 2;
+              title = "Projects";
+              section = "projects";
+              padding = 1;
+              indent = 3;
+            }
+            {
+              pane = 2;
+              icon = " ";
+              title = "Git Status";
+              section = "terminal";
+              enabled.__raw = ''
+                Snacks.git.get_root() ~= nil
+              '';
+              cmd = "${pkgs.hub}/bin/hub status --short --branch --renames";
+              height = 5;
+              padding = 1;
+              ttl = 5 * 60;
+              indent = 3;
             }
           ];
         };
