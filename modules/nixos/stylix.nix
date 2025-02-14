ce32c8af6bd7914c49debe5bba73083ca7752739
AlitPurnama <alitpurnama13@gmail.com>
2025-02-14 22:50:49 +0900

    stylix: change terminal size

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }: {
   stylix.enable = true;
-  stylix.image = ../../wallpapers/dragon.png;
+  stylix.image = ../../images/wallpapers/dragon.png;
   stylix.polarity = "dark";
   stylix.base16Scheme =
     "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-  stylix.fonts = { sizes.terminal = 20; };
+  stylix.fonts = { sizes.terminal = 18; };
   stylix.cursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
     size = 24;
   };
+
 }
