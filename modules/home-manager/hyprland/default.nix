b5683fd8953a5cf617645df71843617774c89eaf
AlitPurnama <alitpurnama13@gmail.com>
2025-02-14 18:55:44 +0900

    add(hyprland): windowrule

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -3,8 +3,12 @@ with lib;
 let
   cfg = config.hyprland;
   bind = (import ./modules/binding.nix { inherit pkgs; }).bind;
+  bindle = (import ./modules/binding.nix { inherit pkgs; }).bindle;
+  bindm = (import ./modules/binding.nix { inherit pkgs; }).bindm;
   exec-once = (import ./modules/exec.nix { inherit pkgs; }).exec-once;
+  windowrulev2 = (import ./modules/windowrule.nix { }).windowrulev2;
 in {
+  imports = [ ./modules/pyprland.nix ];
   options.hyprland.enable = mkEnableOption "Enable Hyprland";
   config = mkIf cfg.enable {
     wayland.windowManager.hyprland = {
@@ -14,7 +18,7 @@ in {
       settings = {
         "$mod" = "SUPER";
         monitor = [ ",preffered,auto,1" ];
-        inherit bind exec-once;
+        inherit bind exec-once bindle bindm windowrulev2;
       };
     };
   };
---
+++ modules/home-manager/hyprland/modules/windowrule.nix
@@ -0,0 +1,10 @@
+{ }: {
+  windowrulev2 = [
+    # XtMapper
+    "float, title:(XtMapper)"
+    "noblur, title:(XtMapper)"
+    "size 100% 100%, title:(XtMapper)"
+    "move 0 0, title:(XtMapper)"
+    "stayfocused, title:(XtMapper)"
+  ];
+}
