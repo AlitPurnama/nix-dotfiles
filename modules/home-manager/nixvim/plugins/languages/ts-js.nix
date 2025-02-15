266ab3fcbcc74eacd738880fe790db95955df029
AlitPurnama <alitpurnama13@gmail.com>
2025-02-15 23:25:45 +0900

    chores(nixvim): disable ts server formatter

--- modules/home-manager/nixvim/plugins/languages/ts-js.nix
+++ modules/home-manager/nixvim/plugins/languages/ts-js.nix
@@ -1,9 +1,9 @@
 { lib, pkgs }: {
   plugins = {
-    lsp.servers = {
-      ts_ls = { enable = true; };
-      none-ls.sources.formatting.prettierd.enable = true;
-    };
+    lsp.servers = { ts_ls = { enable = true; }; };
+
+    none-ls.sources.formatting.prettierd.enable = true;
+    none-ls.sources.formatting.prettierd.disableTsServerFormatter = true;
   };
   keymaps = [ ];
 }
