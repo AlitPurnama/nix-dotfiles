26779a880a551910686aa20b528cd47b1c2a33f9
AlitPurnama <alitpurnama13@gmail.com>
2025-02-15 23:26:52 +0900

    chores(cage-xtmapper): add correct hash

--- packages/cage-xtmapper.nix
+++ packages/cage-xtmapper.nix
@@ -1,14 +1,35 @@
-{ stdenv, fetchurl, autoPatchelfHook }:
-stdenv.mkDerivation (finalAttrs: {
-  pname = "";
+{ lib, stdenv, fetchurl, autoPatchelfHook
+, wayland, libdrm, libxkbcommon, pixman, mesa, vulkan-loader
+, systemd, seatd, xorg, libglvnd
+}:
+
+stdenv.mkDerivation rec {
+  pname = "cage-xtmapper";
   version = "0.2.0";
+
   src = fetchurl {
-    url =
-      "https://github.com/Xtr126/cage-xtmapper/releases/download/latest/cage-xtmapper-v0.2.0.tar";
-    hash = "";
+    url = "https://github.com/Xtr126/cage-xtmapper/releases/download/latest/cage-xtmapper-v0.2.0.tar";
+    hash = "sha256-x1ihGdF2k9S+6e6XRZaS6fK+qJQHfdCA4JTb4VkkNM8=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
+  buildInputs = [
+    wayland
+    libdrm
+    libxkbcommon
+    pixman
+    mesa
+    mesa.drivers
+    vulkan-loader
+    systemd
+    seatd
+    xorg.libxcb
+    xorg.xcbutilrenderutil
+    libglvnd
+  ];
+
+  # Explicitly add mesa driver paths to runtime search
+  runtimeDependencies = [ mesa.drivers ];
 
   sourceRoot = "usr/local/bin";
 
@@ -17,8 +38,11 @@ stdenv.mkDerivation (finalAttrs: {
     install -Dm755 cage_xtmapper cage_xtmapper.sh -t $out/bin/
   '';
 
-  meta = { platforms = stdenv.lib.platforms.linux; 
+  meta = {
+    description = "Cage XtMapper";
+    platforms = lib.platforms.linux;
   };
+
   dontBuild = true;
   dontConfigure = true;
-})
+}
