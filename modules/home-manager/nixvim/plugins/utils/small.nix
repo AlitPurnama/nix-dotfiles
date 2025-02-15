d05b30163cf28f5e56fd7718a3c60c69c7e7c650
AlitPurnama <alitpurnama13@gmail.com>
2025-02-16 01:12:53 +0900

    feat(nixvim/small.nix): add neocord

--- modules/home-manager/nixvim/plugins/utils/small.nix
+++ modules/home-manager/nixvim/plugins/utils/small.nix
@@ -3,7 +3,24 @@
     hardtime.enable = true;
     notify.enable = true;
     noice.enable = true;
+    neocord.enable = true;
   };
 
-  keymaps = [ ];
+  extraPlugins = [
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "showkeys";
+      src = pkgs.fetchFromGitHub {
+        owner = "nvzone";
+        repo = "showkeys";
+        rev = "main";
+        hash = "sha256-0ZONzsCWJzzCYnZpr/O8t9Rmkc4A5+i7X7bkjEk5xmc=";
+      };
+    })
+  ];
+
+  keymaps = [{
+    __unkeyed-1 = "<leader>vk";
+    __unkeyed-2 = "<cmd>ShowkeysToggle<cr>";
+    desc = "Toggle Show Keys (like screencast)";
+  }];
 }
