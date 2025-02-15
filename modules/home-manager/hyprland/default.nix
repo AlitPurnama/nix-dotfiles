9ee4beacf48e4f0509224534fe85b2c21024d283
AlitPurnama <alitpurnama13@gmail.com>
2025-02-15 23:25:30 +0900

    feat(hyprland): import decoration to hyprland

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -6,7 +6,10 @@ let
   bindle = (import ./modules/binding.nix { inherit pkgs; }).bindle;
   bindm = (import ./modules/binding.nix { inherit pkgs; }).bindm;
   exec-once = (import ./modules/exec.nix { inherit pkgs; }).exec-once;
-  windowrulev2 = (import ./modules/windowrule.nix { }).windowrulev2;
+  decoration = (import ./modules/decoration.nix { }).decoration;
+  animations = (import ./modules/animations.nix { }).animations;
+  windowrulev2 =
+    (import ./modules/windowrule.nix { inherit lib; }).windowrulev2;
 in {
   imports = [ ./modules/pyprland.nix ];
   options.hyprland.enable = mkEnableOption "Enable Hyprland";
@@ -18,7 +21,7 @@ in {
       settings = {
         "$mod" = "SUPER";
         monitor = [ ",preffered,auto,1" ];
-        inherit bind exec-once bindle bindm windowrulev2;
+        inherit bind exec-once bindle bindm windowrulev2 decoration animations;
       };
     };
   };
