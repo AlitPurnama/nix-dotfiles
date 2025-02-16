96b4b2b91e8adfc7099bb6bd694c7ae7f9e06ca0
AlitPurnama <alitpurnama13@gmail.com>
2025-02-16 20:46:41 +0900

    feat(hyprpanel): add layouts

--- modules/home-manager/hyprpanel.nix
+++ modules/home-manager/hyprpanel.nix
@@ -1,11 +1,8 @@
-{ inputs, config, lib, ... }:with lib;
-let
-  cfg = config.hyprpanel;
-in
-{
-  imports = [
-    inputs.hyprpanel.homeManagerModules.hyprpanel
-  ];
+{ inputs, config, lib, ... }:
+with lib;
+let cfg = config.hyprpanel;
+in {
+  imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
   options.hyprpanel.enable = mkEnableOption "Enable Hyprpanel";
   config = mkIf cfg.enable {
     programs.hyprpanel = {
@@ -14,6 +11,18 @@ in
       hyprland.enable = true;
       theme = "catppuccin_macchiato_vivid";
       overlay.enable = true;
+
+      layout = {
+        "bar.layouts" = {
+          "0" = {
+            left = [ "dashboard" "workspaces" "windowtitle" ];
+            middle = [ "media" ];
+            right = [ "volume" "network" "systray" "clock" "notifications" ];
+          };
+        };
+      };
+
+      settings = { };
     };
   };
 }
