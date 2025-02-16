23c5e5bffab9798b33f4385ba152ba5c557c5417
AlitPurnama <alitpurnama13@gmail.com>
2025-02-16 20:44:55 +0900

    feat(nixvim/mini): enable some other modules
    - surround
    - etc?

--- modules/home-manager/nixvim/plugins/utils/mini.nix
+++ modules/home-manager/nixvim/plugins/utils/mini.nix
@@ -2,8 +2,79 @@
 
   plugins.mini = {
     enable = true;
-    modules = { icons.enabled = true; };
+    modules = {
+      icons = { };
+      splitjoin = { };
+      comment = {
+        mappings = {
+          comment = "<leader>/";
+          comment_line = "<leader>/";
+          comment_visual = "<leader>/";
+          textobject = "<leader>/";
+        };
+      };
+      surround = {
+        mappings = {
+          add = "gsa";
+          delete = "gsd";
+          find = "gsf";
+          find_left = "gsF";
+          highlight = "gsh";
+          replace = "gsr";
+          update_n_lines = "gsn";
+        };
+      };
+      files = {
+        mappings = {
+          go_in = "<CR>";
+          go_in_plus = "L";
+          go_out = "-";
+          go_out_plus = "H";
+        };
+      };
+    };
     mockDevIcons = true;
   };
-  keymaps = [ ];
+  keymaps = [
+    {
+      __unkeyed-1 = "<leader>ee";
+      __unkeyed-2 = {
+        __raw = # lua
+          ''
+            function()
+                require("mini.files").open()
+            end
+          '';
+      };
+      desc = "Toggle Mini File Explorer";
+
+    }
+    {
+      __unkeyed-1 = "<leader>ef";
+      __unkeyed-2 = {
+        __raw = # lua
+          ''
+            function()
+                MiniFiles = require("mini.files")
+                MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+                MiniFiles.reveal_cwd()
+            end
+          '';
+      };
+      desc = "Toggle into currently opened file";
+    }
+    # Split Join
+    {
+      __unkeyed-1 = "sj";
+      __unkeyed-2 = {
+        __raw = ''
+          function()
+              require("mini.splitjoin").join()
+          end
+        '';
+      };
+      desc = "Join Arguments";
+      mode = [ "n" "x" ];
+    }
+  ];
 }
