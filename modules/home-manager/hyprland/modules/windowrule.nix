d69640732f55a1b4dda543567ef00fb1a1f98921
AlitPurnama <alitpurnama13@gmail.com>
2025-02-16 20:46:18 +0900

    feat(hyprland/windowrule): add opacity for some app

--- modules/home-manager/hyprland/modules/windowrule.nix
+++ modules/home-manager/hyprland/modules/windowrule.nix
@@ -1,12 +1,18 @@
-{ }: {
+{ lib }:
+with lib;
+let
+  blurApps = [ "firefox" "[Cc]ode" "com.mitchellh.ghostty" ];
+  generateOpacityRule = app: "opacity 0.90 0.90,class:^(${app})$";
+in {
   windowrulev2 = [
-    # XtMapper
+    # XtMapper rules
     "float, title:(XtMapper)"
     "noblur, title:(XtMapper)"
     "size 100% 100%, title:(XtMapper)"
     "move 0 0, title:(XtMapper)"
     "stayfocused, title:(XtMapper)"
 
+    # Waydroid rule
     "fullscreen, title:(Waydroid)"
-  ];
+  ] ++ (map generateOpacityRule blurApps);
 }
