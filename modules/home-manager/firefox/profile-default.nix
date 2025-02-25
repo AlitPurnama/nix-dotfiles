71b2f0610fd8729c7c99609ab2a2ed0eb25e900a
AlitPurnama <alitpurnama13@gmail.com>
2025-02-26 04:51:15 +0900

    update flake

---
+++ modules/home-manager/firefox/profile-default.nix
@@ -0,0 +1,13 @@
+{ inputs }: {
+  extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+    bitwarden
+    ublock-origin
+    sponsorblock
+    darkreader
+    auto-tab-discard
+    vimium
+    privacy-badger
+    facebook-container
+    decentraleyes
+  ];
+}
