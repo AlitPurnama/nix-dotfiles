6257448f4b9a4df5ef3d871269f4817143bbee52
AlitPurnama <alitpurnama13@gmail.com>
2025-02-27 19:44:07 +0900

    add(nixvim/dadbod): add dadbod

---
+++ modules/home-manager/nixvim/plugins/utils/dadbod.nix
@@ -0,0 +1,12 @@
+{ lib, pkgs }: {
+  plugins = {
+    vim-dadbod.enable = true;
+    vim-dadbod-completion.enable = true;
+    vim-dadbod-ui.enable = true;
+  };
+  keymaps = [
+    {
+
+    }
+  ];
+}
